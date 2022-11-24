e987f58cc8c59ebbea91e7861ca2b13d64e3b180
Ridwan Akinola <akinolaridwanbnhusaain@gmail.com>
2022-11-24 03:14:26 -0800

    update comment to be more readable

--- utils.c
+++ utils.c
@@ -41,7 +41,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
  *
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 1 if c is a digits,return 0 otherwise
  */
 int is_digit(char c)
 {
